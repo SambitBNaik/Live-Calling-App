432fc0ea26b99e69a9d9a23f0ba0ae2dbbdee367
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-07-20 13:59:34 +0530

    Final Changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "stream-chat-react/dist/css/v2/index.css";
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router';
+import "./index.css";
+import App from "./App.jsx";
+
+import { BrowserRouter } from "react-router";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-      <App />
+        <App />
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
